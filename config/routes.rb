7814176791af5023e8283fa074f8e45a3a3612d3
Jim Chen <jimchen@Jims-iMac.fios-router.home>
2020-08-18 18:59:36 -0400

    roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  roots "welcome#index"
+  root "welcome#index"
   resources :incomes
   resources :spends
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
